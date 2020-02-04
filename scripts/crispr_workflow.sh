1368421d50a7ecdfe54824c594fe35ede867e015
Cecile Hilpert <cecile.hilpert@gmail.com>
2020-02-04 09:44:04 +0100

    New crispr workflow
    Works only for 20 length words

--- scripts/crispr_workflow.sh
+++ scripts/crispr_workflow.sh
@@ -12,66 +12,22 @@
 # NAME_TREE="taxon_tree"
 # URL_TREE_TAXON="http://localhost:2346/"
 
-error_json () {
-    echo "{\"emptySearch\": \"There is a problem, impossible to finish the program\"}" > fail.log
-    cat fail.log
-}
-
-if [ "$pam" != "NGG" ]; then
-    error_json
-
-elif [ "$CRISPR_TOOL_SCRIPT_PATH" = "" ]; then
-    error_json
-
-elif  [ "$URL_CRISPR" = "" ]; then
-    error_json
-
-else
-    slFlag=""
-    #  shorter word
-    if [ "$sl" != "20" ]; then
-        ((_sl = $sl + 3))
-        slFlag="-d 23 -c ${_sl}"
-    fi
-    printenv > env.log
-
-    BASE_FOLDER=`pwd`
-
-    pwd > pwd.log
-
-    #echo "curl -X GET $URL_CRISPR/handshake" > handshake.cmd
-    #curl -X GET $URL_CRISPR/handshake &> handshake.log
-
-    echo $gi > f.gi
-     fileSet="set_index.txt"
-     setCompare $slFlag -i "$gi" -o "$gni" -l $rfg -e index -f $fileSet 2>> ./setCompare.err 1> ./setCompare.log
-
-    echo python -u $CRISPR_TOOL_SCRIPT_PATH/post_processing.py -f $fileSet -sl $sl -pam "NGG" -gi "$gi" -gni "$gni" -r "$URL_CRISPR" -taxon_db "$NAME_TAXON" -tree_db "$NAME_TREE" -end_point "$URL_TREE_TAXON" -c 2000 --no-proxy > pp.cmd
-    python -u $CRISPR_TOOL_SCRIPT_PATH/post_processing.py -f $fileSet -sl $sl -pam "NGG" -gi "$gi" -gni "$gni" -r "$URL_CRISPR" -taxon_db "$NAME_TAXON" -tree_db "$NAME_TREE" -end_point "$URL_TREE_TAXON" -c 2000 --no-proxy 2>> ./post_processing.err 1> ./post_processing.log
-
-
-    if grep "Program terminated" ./post_processing.log > /dev/null;
-    then
-    perl -ne 'if ($_ =~ /Program terminated/){
-        @error_split=split(/&/);
-        $msg = $error_split[1];
-        $msg =~ s/\n$//;
-        print "{\"emptySearch\" :  \"$msg\" }";
-    }' ./post_processing.log > ./fail.log;
-    cat ./fail.log;
-    else
-        not_in=$(perl -ne 'BEGIN{$NR=0};$NR++; if($NR == 1){chomp;$_ =~ s/^[\s]*([\S].*[\S])[\s]*$/$1/;print $_; exit;}' ./post_processing.log);
-        number_hits=$(perl -ne 'BEGIN{$NR=0};$NR++; if($NR == 3){chomp;$_ =~ s/^[\s]*([\S].*[\S])[\s]*$/$1/;print $_; exit;}' ./post_processing.log);
-        tag=$(perl -ne 'BEGIN{$NR=0};$NR++; if($NR == 2){chomp;$_ =~ s/^[\s]*([\S].*[\S])[\s]*$/$1/;print $_; exit;}' ./post_processing.log);
-        echo "$not_in" > ./stuff.log;
-        echo "$number_hits" >> ./stuff.log;
-        echo "$tag" >> ./stuff.log;
-        loc=$(pwd | perl -ne '@tmp = split(/\//, $_); print "$tmp[$#tmp - 1]/$tmp[$#tmp]";');
-    	#number_hits=lines[2].strip()
-        echo "{\"out\" : {\"data_card\": $(cat ./results_by_org.json), \"data\" : $(cat ./results.json),  \"not_in\" : \""$not_in"\",\"gi\" : \""$gi"\",  \"number_hits\" : \""$number_hits"\", \"tag\" : \""$loc"\", \"size\" : $(cat ./size_org.json)}}"
-    fi
-
-
-    #cp -r $WORKDIR/* $BASE_FOLDER
-    #cd $BASE_FOLDER
+#Create sFlag when motif length < 20  
+slFlag=""
+if [ "$sl" != "20" ]; then
+    ((_sl = $sl + 3))
+    slFlag="-d 23 -c ${_sl}"
 fi
+
+fileSet="set_index.txt"
+echo setCompare $slFlag -i "$gi" -o "$gni" -l $rfg -e index -f $fileSet > setCompare.cmd
+setCompare $slFlag -i "$gi" -o "$gni" -l $rfg -e index -f $fileSet 2> ./setCompare.err 1> ./setCompare.log
+
+#echo python -u $CRISPR_TOOL_SCRIPT_PATH/post_processing2.py -f $fileSet -sl $sl -pam "NGG" -gi "$gi" -gni "$gni" -r "$URL_CRISPR" -taxon_db "$NAME_TAXON" -tree_db "$NAME_TREE" --couch_endpoint "$URL_TREE_TAXON" -c 2000 --no-proxy > post_processing.cmd
+
+loc=$(pwd | perl -ne '@tmp = split(/\//, $_); print "$tmp[$#tmp - 1]/$tmp[$#tmp]";');
+
+echo python -u $CRISPR_TOOL_SCRIPT_PATH/post_processing2.py --include "$gi" --exclude "$gni" --couch_endpoint "$COUCH_ENDPOINT" --taxon_db "$NAME_TAXON" --genome_db "$NAME_GENOME" --set_compare set_index.txt > post_processing.cmd
+
+python -u $CRISPR_TOOL_SCRIPT_PATH/post_processing2.py --include "$gi" --exclude "$gni" --couch_endpoint "$COUCH_ENDPOINT" --taxon_db "$NAME_TAXON" --genome_db "$NAME_GENOME" --set_compare set_index.txt --length "$sl" --motif_broker_endpoint "$URL_CRISPR" --tag "$loc" 2>> post_processing.err
+
