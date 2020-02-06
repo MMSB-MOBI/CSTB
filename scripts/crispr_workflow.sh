91f7c2a7b1d11595f01b97b5bd1e1cb3e39c708a
Cecile Hilpert <cecile.hilpert@gmail.com>
2020-02-06 11:42:00 +0100

    crispr_workflow

--- scripts/crispr_workflow.sh
+++ scripts/crispr_workflow.sh
@@ -23,11 +23,9 @@ fileSet="set_index.txt"
 echo setCompare $slFlag -i "$gi" -o "$gni" -l $rfg -e index -f $fileSet > setCompare.cmd
 setCompare $slFlag -i "$gi" -o "$gni" -l $rfg -e index -f $fileSet 2> ./setCompare.err 1> ./setCompare.log
 
-#echo python -u $CRISPR_TOOL_SCRIPT_PATH/post_processing2.py -f $fileSet -sl $sl -pam "NGG" -gi "$gi" -gni "$gni" -r "$URL_CRISPR" -taxon_db "$NAME_TAXON" -tree_db "$NAME_TREE" --couch_endpoint "$URL_TREE_TAXON" -c 2000 --no-proxy > post_processing.cmd
-
 loc=$(pwd | perl -ne '@tmp = split(/\//, $_); print "$tmp[$#tmp - 1]/$tmp[$#tmp]";');
 
-echo python -u $CRISPR_TOOL_SCRIPT_PATH/post_processing2.py --include "$gi" --exclude "$gni" --couch_endpoint "$COUCH_ENDPOINT" --taxon_db "$NAME_TAXON" --genome_db "$NAME_GENOME" --set_compare set_index.txt > post_processing.cmd
+echo python -u $CRISPR_TOOLS_SCRIPT_PATH/post_processing.py --include "$gi" --exclude "$gni" --couch_endpoint "$COUCH_ENDPOINT" --taxon_db "$NAME_TAXON" --genome_db "$NAME_GENOME" --set_compare set_index.txt --length "$sl" --motif_broker_endpoint "$URL_CRISPR" --tag "$loc" > post_processing.cmd
 
-python -u $CRISPR_TOOL_SCRIPT_PATH/post_processing2.py --include "$gi" --exclude "$gni" --couch_endpoint "$COUCH_ENDPOINT" --taxon_db "$NAME_TAXON" --genome_db "$NAME_GENOME" --set_compare set_index.txt --length "$sl" --motif_broker_endpoint "$URL_CRISPR" --tag "$loc" 2>> post_processing.err
+python -u $CRISPR_TOOLS_SCRIPT_PATH/post_processing.py --include "$gi" --exclude "$gni" --couch_endpoint "$COUCH_ENDPOINT" --taxon_db "$NAME_TAXON" --genome_db "$NAME_GENOME" --set_compare set_index.txt --length "$sl" --motif_broker_endpoint "$URL_CRISPR" --tag "$loc" 2>> post_processing.err
 
