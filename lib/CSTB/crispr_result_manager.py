be3b47ded30dd5e49aa7a932fec011d1748f8b38
Cecile Hilpert <cecile.hilpert@gmail.com>
2020-02-06 15:48:44 +0100

    move some scripts and add docstrings

--- lib/CSTB/crispr_result_manager.py
+++ lib/CSTB/crispr_result_manager.py
@@ -2,11 +2,11 @@ import logging
 import pycouch.error
 import re
 from collections import OrderedDict
-import CSTB.crispr_hit.Hit as Hit
-import CSTB.error as error
+from CSTB.engine.crispr_hit import Hit
+import CSTB.utils.error as error
 import requests
 import time
-from typing import List
+from typing import List, Dict, TypedDict
 
 
 SESSION = requests.Session()
@@ -17,16 +17,23 @@ SESSION.trust_env = False
 - make module with wordIntegerIndexing, also use in crispr-manager
 '''
 
-class CrisprResult:
-    def __init__(self, include, exclude):
-        self.genomes_include = include
-        self.genomes_exclude = exclude
+class CrisprResultManager():
+    """Manager for crispr results
+    
+    Attributes:
+        wrapper (pycouch.wrapper.Wrapper): pycouch wrapper to interrogate couchDB
+        motif_broker_endpoint (str): motif-broker endpoint to get sgRNA occurences from couchDB with mapping
+        taxondb (str): taxon database name
+        genomedb (str): genome database name
+        tag (str): tag for result storage
+        nb_total_hits (int): Number of hits found by setCompare. Set with parse_set_compare() call.
+        nb_treated_hits (int): Number of setCompare hits treated by this manager. Set with parse_set_compare() call.
+        hits_collection (List[Hit]) : List of Hit objects
+        include_taxon (Dict) : Dict with included genomes uuid as keys and corresponding taxon name as value
+        exclude_taxon (Dict) : Dict with included genomes uuid as keys and corresponding taxon name as value
 
-    def serialize_genomes_info(self):
-        pass
+    """
 
-
-class CrisprResultManager:
     def __init__(self, pycouch_wrapper, taxondb, genomedb, motif_broker_endpoint, tag):
         self.wrapper = pycouch_wrapper
         self.motif_broker_endpoint = motif_broker_endpoint
@@ -41,9 +48,14 @@ class CrisprResultManager:
 
     #Move this in some consumer
     def get_taxon_name(self, list_uuid):
-        '''
-        Get taxon names from a list of genome uuid. Will interrogate couchDB genome db and taxon db. 
-        '''
+        """Get taxon names for a list of genome uuid. Will interrogate couchDB genome db and taxon db.
+        
+        Args:
+            list_uuid (List[str]): list of genomes uuid
+        
+        Raises:
+            error.CouchNotFound: Raise when couch document is not found
+        """
         correspondance_genome_taxon = {}
         logging.debug(f"Try to get taxon name for {list_uuid}")
         for g_uuid in list_uuid:
@@ -62,6 +74,17 @@ class CrisprResultManager:
 
     #Move this in some consumer
     def get_genomes_size(self, list_uuid):
+        """ Get genome size for a list of genomes uuid. Interrogate couchDB genome database.
+        
+        Args:
+            list_uuid (List[str]): list of genomes uuid
+        
+        Raises:
+            error.CouchNotFound: [description]
+        
+        Returns:
+            Dict: Dict with genome_uuid as key and size dict as value. Size dict contains subsequence name as key and list of coordinates as value. 
+        """
         sizes = {}
         for g_uuid in list_uuid:
             resp = self.wrapper.couchGetDoc(self.genomedb, g_uuid)
@@ -72,12 +95,27 @@ class CrisprResultManager:
         return sizes
             
     def set_taxon_names(self, include, exclude):
+        #type: (List[str], List[str]) -> None
+        """
+        Get taxon names for include and exclude genomes uuid. Call get_taxon_name() and set include_taxon and exclude_taxon attributes. 
+
+        :param include: List of include genome uuid
+        :param exclude: List of exclude genome uuid
+        """
+
         if include:
             self.include_taxon = self.get_taxon_name(include)
         if exclude:
             self.exclude_taxon = self.get_taxon_name(exclude)
 
     def search_occurences(self, genomes_include, len_slice = 2000):
+        #type: (List[str], int) -> None
+        """Search sgRNA occurences in couchDB with motif broker and complete Hit objects.
+        
+        :param genomes_include: list of genome uuid
+        :param len_slice: Length of packet to interrogate couchDB, defaults to 2000
+        :raises error.PingError: Raise when motif-broker can't be reach
+        """
         try:
             SESSION.get(self.motif_broker_endpoint + "/handshake")
         except:
@@ -103,6 +141,16 @@ class CrisprResultManager:
            
 
     def parse_set_compare(self, setCompare_file, word_length, to_keep):
+        """ Parse results from setCompare. Will call a different parsing function for word with size 20 and for shorter words. Separate functions are required because setCompare results format is not the same with with 20-length words and shorter words. Initialize hits_collection and nb_treated_hits
+        
+        :param setCompare_file: path to setCompare result file 
+        :type setCompare_file: str
+        :param word_length: Word length
+        :type word_length: int
+        :param to_keep: Number of hits to keep
+        :type to_keep: int
+        """
+        
         self.nb_treated_hits = to_keep
         self.hits_collection=[]
         logging.info(f"Parse set compare for word length {word_length}")
@@ -112,6 +160,14 @@ class CrisprResultManager:
             logging.debug("Handle this")
             
     def _parse_set_compare_20(self, setCompare_file, to_keep):
+        """Parse setCompare for word of size 20. 
+        
+        :param setCompare_file: path to setCompare result file 
+        :type setCompare_file: str
+        :param to_keep: Number of hits to keep
+        :type to_keep: int
+        """
+
         with open(setCompare_file, "r") as filin:
             text = filin.readlines()
 
@@ -148,6 +204,12 @@ class CrisprResultManager:
         return index_dic, nb_hits
 
     def generate_json_data(self):
+        """Generate json data for client (to display into table) from Hit collection. 
+        
+        :return: json data, 
+        :rtype: List[Dict]
+        """
+
         results = []
         sorted_hits = sorted(self.hits_collection, key=lambda hit: hit.number_occurences)
         #logging.debug([hit.number_occurences for hit in self.hits_collection])
@@ -158,6 +220,11 @@ class CrisprResultManager:
         return results
 
     def generate_json_data_card(self):
+        """Generate json data for client to display genomic card from Hit collection. 
+        
+        :return: json data
+        :rtype: Dict { organism : { fasta_header : { sgRNA_sequence : List[str] } } }
+        """
 
         def insert_in_data_card(genome_uuid, subseq, sequence, coords):
             genome_name = self.include_taxon[genome_uuid]
@@ -178,6 +245,11 @@ class CrisprResultManager:
         return data_card
 
     def generate_json_size(self):
+        """[summary]
+        
+        :return: json data
+        :rtype: Dict { organism : { fasta_header : length } }
+        """
         json_size = {}
         sizes = self.get_genomes_size(list(self.include_taxon.keys()))
         for g_uuid in sizes:
@@ -186,6 +258,11 @@ class CrisprResultManager:
         return json_size
         
     def format_results(self):
+        """Create the final json for client
+        
+        :return: final json
+        :rtype: Dict
+        """
         final_json = {}
         final_json["gi"] = "&".join(list(self.include_taxon.values()))
         final_json["not_in"] = ",".join(list(self.exclude_taxon.values()))
--- lib/CSTB/engine/crispr_hit.py
+++ lib/CSTB/engine/crispr_hit.py
@@ -29,43 +29,57 @@ word    ref : coordinates   ref : coordinates   ref : coordinates...
 import sys
 import json
 import itertools
-import CSTB.wordIntegerIndexing as decoding
+import CSTB.engine.wordIntegerIndexing as decoding
 import logging
-import CSTB.error as error
+import CSTB.utils.error as error
 
 
 
 class Hit():
-    """
-    Object Hit containing the CRISPR sequence, indexes where it is found
-    and its associated score
+    """Hit object
+    
+    :ivar index: setCompare index
+    :vartype index: int
+    :ivar weight: setCompare weigth, correspond to number of sgRNA occurences in all genomes
+    :vartype weight: int
+    :ivar sequence: sgRNA sequence in nucleotides
+    :vartype sequence: str
+    :ivar occurences: Occurences of sgRNA in genomes
+    :vartype occurences: Dict { genome : { fasta_header : List[str] } }
+   
     """
     def __init__(self, index, weight):
+        """ Initialize an Hit object
+        
+        :param index: setCompare index
+        :type index: int
+        :param weight: setCompare weight, correspond to number of sgRNA occurences in all genomes
+        :type weight: int
+        """
         self.index = int(index)
         self.weight = weight
         self.sequence = self.decode()
         self.occurences = {} #This dictionnary will be {"organism": {"subsequence" : coords[]}}
 
-    def set_genomes_dict(self, dic_seq):
-        self.genomes_dict = dic_seq
-
-    def write(self, genomes_in):
-        to_write = ""
-        for gi in genomes_in:
-            for ref in self.genomes_dict[gi]:
-                to_write += ref + ':' + ','.join(self.genomes_dict[gi][ref]) + ';'
-        return to_write.strip(';')
-
-    def list_ref(self, org_name):
+    def _list_ref(self, org_name):
+        """Format occurences for an organism 
+        """
         list_ref = [{"ref": ref, "coords": self.occurences[org_name][ref]} for ref in self.occurences[org_name]]
         return list_ref
 
     def list_occ(self, taxon_name):
-        list_occurences = [{'org': taxon_name[genome], 'all_ref': self.list_ref(genome)} for genome in self.occurences]
+        """Format occurences for an organism
+        """
+        list_occurences = [{'org': taxon_name[genome], 'all_ref': self._list_ref(genome)} for genome in self.occurences]
         return list_occurences
 
     @property
     def number_occurences(self):
+        """Give the total number of hit occurences
+        
+        :return: Total number of hit occurences
+        :rtype: int
+        """
         nb_occ = 0
         for genome in self.occurences:
             for subseq in self.occurences[genome]:
@@ -76,6 +90,14 @@ class Hit():
         return f"index:{self.index}\nweight:{self.weight}\nsequence:{self.sequence}\noccurences:{self.occurences}"
 
     def store_occurences(self, couch_doc, genomes_in):
+        """Set occurences attributes from couch document
+        
+        :param couch_doc: couch response
+        :type couch_doc: Dict
+        :param genomes_in: list of genomes uuid
+        :type genomes_in: List[str]
+        :raises error.ConsistencyError: Raise if at least 1 given genome is not in couch document. 
+        """
         db_genomes = set(couch_doc.keys())
         logging.debug(f"store_occurences\ncouch_doc: {couch_doc}\ngenomes_in: {genomes_in}")
         if not set(genomes_in).issubset(db_genomes):
@@ -84,14 +106,12 @@ class Hit():
         self.occurences = {genome:couch_doc[genome] for genome in genomes_in}
 
     def decode(self):
+        """Decode setCompare index into nucleotide sequence
+        
+        :return: sgRNA sequence
+        :rtype: str
+        """
         return decoding.decode(self.index, ["A", "T", "C", "G"], 23)
-            
-
-def eprint(*args, **kwargs):
-    """
-    For printing only on the terminal
-    """
-    print(*args, file=sys.stderr, **kwargs)
 
 
 def write_to_file(genomes_in, genomes_not_in, dic_hits, pam, non_pam_motif_length, workdir, nb_top, hit_obj, list_ordered):
@@ -100,6 +120,8 @@ def write_to_file(genomes_in, genomes_not_in, dic_hits, pam, non_pam_motif_lengt
     The file is a tabulated file, with first column=sgrna sequence,
     then one column for each included organisms with list of positions
     of the sequence in each.
+
+    OBSOLETE, maybe we need this if we keep the option to download results.
     """
     eprint('\n\n-- Write results to file --')
     rep_rslt_file = workdir + '/results_allgenome.txt'
@@ -124,68 +146,3 @@ def write_to_file(genomes_in, genomes_not_in, dic_hits, pam, non_pam_motif_lengt
         to_write = hit.write(genomes_in)
         output.write(to_write + '\n')
     output.close()
-
-
-def create_list_ref(dic_ref):
-    """
-    Create a list of references
-    """
-    list_ref = []
-    for ref in dic_ref:
-        dic = {"ref": ref, "coords": dic_ref[ref]}
-        list_ref.append(dic)
-    return list_ref
-
-
-def create_list_occurences(dic_occurences):
-    """
-    Create a list of occurences
-    """
-    list_occurences = []
-    for genome in dic_occurences:
-        dic_genome = {'org': genome, 'all_ref': create_list_ref(dic_occurences[genome])}
-        list_occurences.append(dic_genome)
-    return list_occurences
-
-
-def output_interface(dic_hits, workdir, nb_top):
-    """
-    Reformat the results to create a json file.
-    It will be parsed in javascript to display it in interface.
-    """
-    # eprint('\n\n-- Construct results for graphical interface --')
-    json_result_file = workdir + '/results.json'
-    list_dic = [{'sequence': sgrna, 'occurences':dic_hits[sgrna].list_occ()} for i, sgrna in itertools.takewhile(lambda x: x[0] < nb_top, enumerate(dic_hits)) ]
-    list_dic.reverse()
-    with open(json_result_file, 'w') as filout:
-        json.dump(list_dic, filout, indent=4)
-
-
-def display_hits(dic_hits, genomes_in, genomes_not_in, pam, non_pam_motif_length, workdir, nb_top, hit_obj, list_ordered):
-    """
-    write output for interface
-    """
-    # Put results in local file for access via the interface.
-    write_to_file(genomes_in, genomes_not_in, dic_hits, pam,
-                  non_pam_motif_length, workdir, nb_top, hit_obj, list_ordered)
-
-    # Output formatting for printing to interface
-    output_interface(dic_hits, workdir, 100)
-
-    dic_sorted_by_org = {genome : {} for genome in genomes_in}
-    if hit_obj:
-        for seq in dic_hits:
-            for genome in genomes_in:
-                for ref in dic_hits[seq].genomes_dict[genome]:
-                    if ref not in dic_sorted_by_org[genome]:
-                        dic_sorted_by_org[genome][ref] = {}
-                    dic_sorted_by_org[genome][ref][seq] = dic_hits[seq].genomes_dict[genome][ref]
-    else:
-        for seq in dic_hits:
-            for genome in genomes_in:
-                for coord_seq in dic_hits[seq].dic_org[genome]:
-                    ref = coord_seq.ref
-                    if ref not in dic_sorted_by_org[genome]:
-                        dic_sorted_by_org[genome][ref] = {}
-                    dic_sorted_by_org[genome][ref][seq] = coord_seq.list_coord
-    json.dump(dic_sorted_by_org, open("results_by_org.json", "w"))
