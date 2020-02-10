75f33ab02e6b1f2f762b3717cd6d900cb02c29d6
Cecile Hilpert <cecile.hilpert@gmail.com>
2020-02-10 16:44:44 +0100

    bash workflow for specific gene

--- scripts/crispr_workflow_specific.sh
+++ scripts/crispr_workflow_specific.sh
@@ -14,118 +14,32 @@
 # fileBlast="../crispr/test/data/sg/blast.xml"
 # pid=70
 
+set -e
 
-error_json () {
-    echo "{\"emptySearch\": \"There is a problem, impossible to finish the program\"}" > fail.log
-    cat fail.log
-}
+queryFasta="query.fasta"
+printf ">query\n$seq\n" > $queryFasta
+queryIndex="query.index"
+#Search and index sgRNA in gene
+echo python -u $CRISPR_TOOLS_SCRIPT_PATH/index_sequence.py -f $squeryFasta -o $queryIndex > index_query.cmd
+python -u $CRISPR_TOOLS_SCRIPT_PATH/index_sequence.py -f $queryFasta -o $queryIndex 2> index_query.err
 
-parse_logFile () {
-    if grep "Program terminated" $1 > /dev/null;
-    then
-        perl -ne 'if ($_ =~ /Program terminated/){
-            @error_split=split(/&/);
-            $msg = $error_split[1];
-            $msg =~ s/\n$//;
-            print "{\"emptySearch\" :  \"$msg\" }";
-        }' $1 > ./fail.log;
-        cat ./fail.log;
-        false
-    else
-         true
-    fi
-}
-
-if [ "$pam" != "NGG" ]; then
-    error_json
-
-#elif [ "$sl" != "20" ]; then
-#    error_json
-
-elif [ "$CRISPR_TOOL_SCRIPT_PATH" = "" ]; then
-    error_json
-
-elif  [ "$URL_CRISPR" = "" ]; then
-    error_json
-
-else
-
-    slFlag=""
-    #  shorter word
-    if [ "$sl" != "20" ]; then
-        ((_sl = $sl + 3))
-        slFlag="-d 23 -c ${_sl}"
-    fi
-    printenv > env.log
-
-    BASE_FOLDER=`pwd`
-
-    pwd > pwd.log
-
-    ## CREATE METAFILE ###
-    queryFasta="query.fasta"
-    printf ">query\n$seq\n" > $queryFasta
-
-    metafileQuery="query"
-    echo python -u $CRISPR_TOOL_SCRIPT_PATH/create_metafile.py -file $queryFasta -out $metafileQuery > sg.cmd
-    python -u $CRISPR_TOOL_SCRIPT_PATH/create_metafile.py -file $queryFasta -out $metafileQuery 2>> ./metafile.err 1>./metafile.log
-    # Check if sgRNA on the query gene
-    parse_logFile ./metafile.log
-    PRG_TERMINATED=$?
-    if [ $PRG_TERMINATED = 0 ];then
-        ### FILTER GENOMES ###
-       # gi=$(python $CRISPR_TOOL_SCRIPT_PATH/filter_specie.py --ref $SPECIE_REF_JSON --query "$gi")
-       # gni=$(python $CRISPR_TOOL_SCRIPT_PATH/filter_specie.py --ref $SPECIE_REF_JSON --query "$gni")
-        echo $gi > f.gi
-
-        ### INTERSECTION ###
-        fileSet="set_index.txt"
-        setCompare $slFlag -i "$gi" -o "$gni" -l $rfg -e index -f $fileSet -s $metafileQuery".index" 2>> ./setCompare.err 1> ./setCompare.log
-
-        nb_hits=`sed -n "s/^# \([0-9]*\).*/\1/p" $fileSet`
-    fi
-    # Check if hits in intersection
-    if [ $nb_hits = 0 ];then
-        echo "{\"emptySearch\" :  \"No common hits between the sequence and included genomes \"}"  > ./fail.log
-        cat ./fail.log
-        PRG_TERMINATED=1
-    fi
-
-    ### BLAST N TO FIND HOMOLOGOUS GENES ###
-    if [ $PRG_TERMINATED = 0 ];then
-        fileBlast="blast_output.xml"
-        echo blastn -outfmt 5 -query $queryFasta -db $blastdb > $fileBlast >> sg.cmd
-        blastn -outfmt 5 -query $queryFasta -db $blastdb > $fileBlast
-
-        ### PARSE BLAST OUTPUT ###
-        parseBlast="parse_blast.p"
-        echo python -u $CRISPR_TOOL_SCRIPT_PATH/parse_blast.py -blast $fileBlast -gi "$gi" -o $parseBlast -ip $pid >> sg.cmd
-        python -u $CRISPR_TOOL_SCRIPT_PATH/parse_blast.py -blast $fileBlast -gi "$gi" -o $parseBlast -ip $pid 2>> ./parse_blast.err 1> ./parse_blast.log
-        # check if hits
-        parse_logFile ./parse_blast.log
-        PRG_TERMINATED=$?
-    fi
-
-    ### CHECK IG SGRNA ARE ON HOMOLOGOUS GENES ###
-    if [ $PRG_TERMINATED = 0 ];then
-        # Post-processing with setCompare output and blast output
-        echo python -u $CRISPR_TOOL_SCRIPT_PATH/specific_gene.py -f $fileSet -sl $sl -pam "NGG" -gi "$gi" -gni "$gni" -r "$URL_CRISPR" -taxon_db "$NAME_TAXON" -tree_db "$NAME_TREE" -end_point "$URL_TREE_TAXON" -c 2000 --no-proxy -blast $parseBlast >> sg.cmd
-        python -u $CRISPR_TOOL_SCRIPT_PATH/specific_gene.py -f $fileSet -sl $sl -pam "NGG" -gi "$gi" -gni "$gni" -r "$URL_CRISPR"  -taxon_db "$NAME_TAXON" -tree_db "$NAME_TREE" -end_point "$URL_TREE_TAXON" -c 2000 --no-proxy -blast $parseBlast 2>> ./specific_gene.err 1> ./specific_gene.log
-        # Check if exist sgrna on genes
-        parse_logFile ./specific_gene.log
-        PRG_TERMINATED=$?
-    fi
-
-    if [ $PRG_TERMINATED = 0 ];then
-        not_in=$(perl -ne 'BEGIN{$NR=0};$NR++; if($NR == 1){chomp;$_ =~ s/^[\s]*([\S].*[\S])[\s]*$/$1/;print $_; exit;}' ./specific_gene.log);
-        number_hits=$(perl -ne 'BEGIN{$NR=0};$NR++; if($NR == 3){chomp;$_ =~ s/^[\s]*([\S].*[\S])[\s]*$/$1/;print $_; exit;}' ./specific_gene.log);
-        tag=$(perl -ne 'BEGIN{$NR=0};$NR++; if($NR == 2){chomp;$_ =~ s/^[\s]*([\S].*[\S])[\s]*$/$1/;print $_; exit;}' ./specific_gene.log);
-
-        echo "$not_in" > ./stuff.log;
-        echo "$number_hits" >> ./stuff.log;
-        echo "$tag" >> ./stuff.log;
-        loc=$(pwd | perl -ne '@tmp = split(/\//, $_); print "$tmp[$#tmp - 1]/$tmp[$#tmp]";');
-        echo "{\"out\" : {\"data_card\": $(cat ./results_by_org.json), \"data\" : $(cat ./results.json),  \"not_in\" : \""$not_in"\",\"gi\" : \""$gi"\",  \"gene\" : $(cat ./genes.json), \"number_hits\" : \""$number_hits"\", \"tag\" : \""$loc"\", \"size\" : $(cat ./size_org.json)}}"
-
-    fi
+slFlag=""
+if [ "$sl" != "20" ]; then
+    ((_sl = $sl + 3))
+    slFlag="-d 23 -c ${_sl}"
 fi
+
+fileSet="set_index.txt"
+echo setCompare $slFlag -i "$gi" -o "$gni" -l $rfg -e index -f $fileSet -s $queryIndex > setCompare.cmd
+setCompare $slFlag -i "$gi" -o "$gni" -l $rfg -e index -f $fileSet -s $queryIndex 2> ./setCompare.err 1> ./setCompare.log
+
+nb_hits=`sed -n "s/^# \([0-9]*\).*/\1/p" $fileSet`
+
+blastOutput="blast_output.xml"
+echo blastn -outfmt 5 -query $queryFasta -db $blastdb > blast.cmd
+blastn -outfmt 5 -query $queryFasta -db $blastdb > $blastOutput
+
+echo python -u $CRISPR_TOOLS_SCRIPT_PATH/post_processing.py --include "$gi" --exclude "$gni" --couch_endpoint "$COUCH_ENDPOINT" --taxon_db "$NAME_TAXON" --genome_db "$NAME_GENOME" --set_compare set_index.txt --length "$sl" --motif_broker_endpoint "$URL_CRISPR" --tag "$loc" --blast $blastOutput > post_processing.cmd
+
+python -u $CRISPR_TOOLS_SCRIPT_PATH/post_processing.py --include "$gi" --exclude "$gni" --couch_endpoint "$COUCH_ENDPOINT" --taxon_db "$NAME_TAXON" --genome_db "$NAME_GENOME" --set_compare set_index.txt --length "$sl" --motif_broker_endpoint "$URL_CRISPR" --tag "$loc" --blast $blastOutput 2> post_processing.err
+
