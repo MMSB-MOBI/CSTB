36b7bf2d798b0561c603dec60086a90af01a17da
Cecile Hilpert <cecile.hilpert@gmail.com>
2020-02-13 11:15:36 +0100

    handle bash error

--- lib/CSTB/utils/error.py
+++ lib/CSTB/utils/error.py
@@ -1,4 +1,5 @@
 import json
+import traceback
 
 class CouchNotFound(Exception):
     pass
--- scripts/crispr_workflow.sh
+++ scripts/crispr_workflow.sh
@@ -12,20 +12,38 @@
 # NAME_TREE="taxon_tree"
 # URL_TREE_TAXON="http://localhost:2346/"
 
+run_setCompare() {
+    slFlag=""
+    if [ "$sl" != "20" ]; then
+        ((_sl = $sl + 3))
+        slFlag="-d 23 -c ${_sl}"
+    fi
+    fileSet="set_index.txt"
+    echo setCompare $slFlag -i "$gi" -o "$gni" -l $rfg -e index -f $fileSet > setCompare.cmd
+    setCompare $slFlag -i "$gi" -o "$gni" -l $rfg -e index -f $fileSet 2> ./setCompare.err 1> ./setCompare.log
+}
+
+run_post_processing(){
+    if [[ -s $fileSet ]]; then
+        loc=$(pwd | perl -ne '@tmp = split(/\//, $_); print "$tmp[$#tmp - 1]/$tmp[$#tmp]";');
+        echo python -u $CRISPR_TOOLS_SCRIPT_PATH/post_processing.py --include "$gi" --exclude "$gni" --couch_endpoint "$COUCH_ENDPOINT" --taxon_db "$NAME_TAXON" --genome_db "$NAME_GENOME" --set_compare set_index.txt --length "$sl" --motif_broker_endpoint "$MOTIF_BROKER_ENDPOINT" --tag "$loc" > post_processing.cmd
+        python -u $CRISPR_TOOLS_SCRIPT_PATH/post_processing.py --include "$gi" --exclude "$gni" --couch_endpoint "$COUCH_ENDPOINT" --taxon_db "$NAME_TAXON" --genome_db "$NAME_GENOME" --set_compare set_index.txt --length "$sl" --motif_broker_endpoint "$MOTIF_BROKER_ENDPOINT" --tag "$loc" 2>> post_processing.err
+    fi
+}
+
+error_json () {
+    msg=$1
+    echo "{\"error\": \"$1\"}"
+}
+
 #Create sFlag when motif length < 20  
-slFlag=""
-if [ "$sl" != "20" ]; then
-    ((_sl = $sl + 3))
-    slFlag="-d 23 -c ${_sl}"
+
+run_setCompare
+if [[ -s setCompare.err ]]; then
+    error_json "Error while setCompare"
+else 
+    run_post_processing
 fi
 
-fileSet="set_index.txt"
-echo setCompare $slFlag -i "$gi" -o "$gni" -l $rfg -e index -f $fileSet > setCompare.cmd
-setCompare $slFlag -i "$gi" -o "$gni" -l $rfg -e index -f $fileSet 2> ./setCompare.err 1> ./setCompare.log
 
-loc=$(pwd | perl -ne '@tmp = split(/\//, $_); print "$tmp[$#tmp - 1]/$tmp[$#tmp]";');
-
-echo python -u $CRISPR_TOOLS_SCRIPT_PATH/post_processing.py --include "$gi" --exclude "$gni" --couch_endpoint "$COUCH_ENDPOINT" --taxon_db "$NAME_TAXON" --genome_db "$NAME_GENOME" --set_compare set_index.txt --length "$sl" --motif_broker_endpoint "$MOTIF_BROKER_ENDPOINT" --tag "$loc" > post_processing.cmd
-
-python -u $CRISPR_TOOLS_SCRIPT_PATH/post_processing.py --include "$gi" --exclude "$gni" --couch_endpoint "$COUCH_ENDPOINT" --taxon_db "$NAME_TAXON" --genome_db "$NAME_GENOME" --set_compare set_index.txt --length "$sl" --motif_broker_endpoint "$MOTIF_BROKER_ENDPOINT" --tag "$loc" 2>> post_processing.err
 
